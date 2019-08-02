2d8fd07fffdc41db69dc0804eed6be416188bbe3
zoekao <kaoxx085@umn.edu>
2019-08-01 21:58:24 -0500

    some modification

--- powerlawCalc.R
+++ powerlawCalc.R
@@ -12,14 +12,14 @@ estAlpha = function(x, alpha, m) {
 
 alpha_seq = seq(0.1, 5, 0.01)
 
-out = do.call(rbind, lapply(alpha_seq, estAlpha, x = x, m = 4*30))
+out = do.call(rbind, lapply(alpha_seq, estAlpha, x = x, m = 4*50))
 out = data.frame(out)
 min_alpha = out$alpha[out$difference == min(out$difference)]
 
-PowerLawDistribution = dpldis(x, 1, 0.77)
+PowerLawDistribution = dpldis(x, 1, 0.36)
 PowerLawDist = (PowerLawDistribution)/sum(PowerLawDistribution)
 
-fileConn<-file("PowerLawDist(120degree).txt")
+fileConn<-file("PowerLawDist(200degree).txt")
 writeLines(paste0(PowerLawDist, collapse=", "), fileConn)
 close(fileConn)
 
